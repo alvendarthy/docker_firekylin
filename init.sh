e668834ca5eac86204ae4fcdc4d0411100a02fae
alvendarthy <alvendarthy@gmail.com>
2019-11-02 04:49:06 +0000

    fix init.sh sed bug

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ read -p "your domain:(like baidu.com, without www): " DOMAIN
 read -t 10 -p "Now press ENTER to set domain."
 
 echo "changging ./nginx/nginx.conf"
-sed 's/HOST/'"$DOMAIN"'/g' ./nginx/nginx.conf
+sed -i 's/HOST/'"$DOMAIN"'/g' ./nginx/nginx.conf
 
 echo -i "changging ./isso/config/isso.conf"
 sed -i 's/HOST/'"$DOMAIN"'/g' ./isso/config/isso.conf
